2af18655fe74f5e82bc37ba48084573fa866c70c
Sixing <sixing.xv@gmail.com>
2022-08-25 19:23:14 +0800

    preview designs, new status design

---
+++ preview-2/main.js
@@ -0,0 +1,87 @@
+var mintedArr = [];
+var orderArr = [];
+
+
+$( document ).ready(function() {
+
+    $.getJSON("test-list.json", function(data) {
+        eps = data;
+        eps.forEach(item=>{
+            if(item.Status=='Minted'){
+                console.log(item.Status)
+                mintedArr.push(item)
+            }
+        })
+        orderArr = Array.from(Array(mintedArr.length).keys())
+
+
+            // setInterval(()=>{
+            //     $('.logo').fadeIn('slow');
+            //     $('.nft-status').css('filter','blur(50px)')
+            //     $('.nft-status').css('-webkit-filter','blur(50px)')
+            // },17000)
+    
+            // setInterval(()=>{
+            //     $('.logo').fadeOut('slow');
+            //     $('.nft-status').css('filter','blur(0px)')
+            //     $('.nft-status').css('-webkit-filter','blur(0px)')
+            // },20000)
+ 
+
+
+        orderArr.forEach(item=>{
+            var currentIndex = orderArr.indexOf(item)
+            //$('.name-display').append(`<span class="visitor-name" id="visitor-${mintedArr.indexOf(item)}">${item.Visitor}</span>`)
+            //$('.visitor-name').first().show();
+            if((currentIndex+1)%3 ==0){
+                    $('.splide__list').append(`
+                    <li class="splide__slide" data-splide-interval="8000">
+                    <div class="visitor-name" id="visitor-${orderArr.indexOf(item)}"><b>${mintedArr[currentIndex].Visitor}'s TRUE NAME:</b></div>
+                    <div class="nft-image">
+                    <img src="imgs/truename-mockup.jpg">
+                    </div></li>`)  
+            }else{
+                $('.splide__list').append(`
+                <li class="splide__slide" data-splide-interval="8000">
+                <div class="visitor-name" id="visitor-${orderArr.indexOf(item)}"><b>${mintedArr[currentIndex].Visitor}'s TRUE NAME:</b></div>
+                <div class="nft-image">
+                <img src="imgs/truename-mockup.jpg">
+                </div></li>`)
+            }
+
+
+            // $('.nft-image').first().show();
+
+        })
+
+        var splide = new Splide( '.splide', {
+            type   : 'loop',
+            perPage: 1,
+            autoplay:true,
+            cover:true,
+            pagination:false,
+            arrows:false,
+          } );
+        // splide.on( 'autoplay:playing', function ( rate ) {
+        //     console.log( rate ); // 0-1
+        //   } );
+         splide.mount();
+
+    })
+
+    console.log(mintedArr)
+
+ })
+
+function imageShuffle(){
+        orderArr.forEach(item=>{
+            console.log(item)
+             setTimeout(function(){
+                $(`#visitor-${item}`).slideToggle('slow')
+                $(`#visitor-${item+1}`).slideToggle('slow')
+                $(`#nft-${item}`).slideToggle('slow')
+                $(`#nft-${item+1}`).slideToggle('slow')
+            },  2000)
+        })
+
+}
---
+++ status-1/main.js
@@ -0,0 +1,106 @@
+
+
+$( document ).ready(function() {
+    $.getJSON("test-list.json", function(data) {
+        eps = data;
+
+        displayStatus()
+
+        
+        var Arr = Array.from(Array(40).keys())
+        var randomArr = shuffleArr(Arr)
+        console.log(randomArr)
+
+        // shuffle the display every 8 seconds
+        setInterval(()=>{
+            randomArr.forEach(item=>{
+                console.log(item)
+                 setTimeout(function(){
+                    $(`#entry-${item}`).slideToggle('slow')
+                    $(`#entry-${item+40}`).slideToggle('slow')
+                },  Math.floor(Math.random() * 2000))
+            })
+        }, 8000)
+
+
+
+    })
+
+    
+
+
+ })
+
+
+//  function displayStatus(round){
+//     $('.arrival-screen').append(`<div class='batch-${round}'></div>`)
+//     for (var i = 50*(round-1); i<50*round;i++){
+//         $(`.batch-${round}`).append(`<div class="entry" id="entry-${i}"></div>`)
+//         $(`#entry-${i}`).append(`<div class="time" id="time-${i}">${eps[i].Time}</div>`)
+//         $(`#entry-${i}`).append(`<div class="visitor" id="visitor-${i}">${eps[i].Visitor}</div>`)
+//         $(`#entry-${i}`).append(`<div class="status" id="status-${i}">${eps[i].Status}</div>`)
+//        if(eps[i].Status=='Viewed'){
+//            $(`#entry-${i}`).addClass('viewed')
+//        }
+//        if(eps[i].Status=='Minted'){
+//            $(`#entry-${i}`).addClass('minted')
+//        }
+//        if(i==50*round-1){
+//            $(`#entry-${i}`).addClass('last-entry')
+//        }
+//     }
+//  }
+
+ function displayStatus(){
+    $('.arrival-screen').append(`<div class='batch'></div>`)
+    for (var i = 0; i<40;i++){
+        $(`.batch`).append(`<div class="entry" id="entry-${i}"></div>`)
+        $(`.batch`).append(`<div class="entry" id="entry-${i+40}"></div>`)
+        $(`#entry-${i+40}`).hide()
+        $(`#entry-${i}`).append(`<div class="time" id="time-${i}">${eps[i].Time}</div>`)
+        $(`#entry-${i+40}`).append(`<div class="time" id="time-${i+40}">${eps[i+40].Time}</div>`)
+        $(`#entry-${i}`).append(`<div class="visitor" id="visitor-${i}">${eps[i].Visitor}</div>`)
+        $(`#entry-${i+40}`).append(`<div class="visitor" id="visitor-${i+40}">${eps[i+40].Visitor}</div>`)
+        $(`#entry-${i}`).append(`<div class="status" id="status-${i}">${eps[i].Status}</div>`)
+        $(`#entry-${i+40}`).append(`<div class="status" id="status-${i}">${eps[i+40].Status}</div>`)
+
+       if(eps[i].Status=='Viewed'){
+           $(`#entry-${i}`).addClass('viewed')
+       }
+       if(eps[i].Status=='Minted'){
+           $(`#entry-${i}`).addClass('minted')
+       }
+
+       if(eps[i+50].Status=='Viewed'){
+        $(`#entry-${i+40}`).addClass('viewed')
+    }
+    if(eps[i+50].Status=='Minted'){
+        $(`#entry-${i+40}`).addClass('minted')
+    }
+       if(i==39){
+        $(`#entry-${i}`).addClass('last-entry')
+           $(`#entry-${i+40}`).addClass('last-entry')
+       }
+    }
+ }
+
+ function shuffleArr(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+ 
